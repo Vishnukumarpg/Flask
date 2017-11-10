9ff35db1fba9d60dae496e5966d56bfa507bf496
Vishnukumarpg <33220068+Vishnukumarpg@users.noreply.github.com>
2017-11-10 14:47:08 +0530

    Add files via upload

--- myflask_test.py
+++ myflask_test.py
@@ -1,36 +1,30 @@
 from flask import Flask, request, Response
 
-class MyResponse(Response):
-    default_mimetype = 'application/xml'
-class MyFlask(Flask):
-    response_class = MyResponse
-
-app = MyFlask(__name__)
+app = Flask(__name__)
 
 
 @app.route('/')
 def display():
     return "Looks like it works!!"
 
-@app.route('/alpha')
-def alpha():
-    return "This is the aplha version !!"
 
-@app.route('/number')
-def input():
-    if 'src' in request.args and 'dst' in request.args :
-        return "The source of the request is {} and destination is {} ". format(request.args.get('src'), request.args.get('dst'))
+@app.route('/fwdxml', methods=['GET'])
+def xml_gen():
+    if 'src' in request.args and 'dst' in request.args and 'text' in request.args and 'fwd_num' in request.args:
+        src = request.args.get('src')
+        first_dst = request.args.get('dst')
+        text = request.args.get('text')
+        fwd_num = request.args.get('fwd_num')
+    else :
+        return "Message details were not recieved properly !"
 
-@app.route('/beta', methods=['POST'])
-def xml():
-    if 'src' in request.args and 'dst' in request.args and 'text' in request.args:
-        pass
-    src = request.args.get('src')
-    dst = request.args.get('dst')
-    text = request.args.get('text')
+    print "Message is reieved from source {} and to first destination {}".format(src,first_dst)
 
-    return '''<Response><Message callbackMethod="POST" callbackUrl="https://requestb.in/1md8ubo1" dst="{}" src="{}" type="sms">{}</Message></Response>'''.format(dst,src,text)
+    print "Message will be forwarded from first_destination -> {} to final_destination --> {}".format(first_dst,fwd_num)
+
+    xml = '''<Response><Message callbackMethod="POST" callbackUrl="https://requestb.in/1md8ubo1" dst="{}" src="{}" type="sms">{}</Message></Response>'''.format(fwd_num,first_dst,text)
+    return Response(xml, mimetype='text/xml')
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=3134)
+    app.run(debug=True, port=3135)
